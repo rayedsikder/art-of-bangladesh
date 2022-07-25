1027a76b4f0357da386bc230d3fc15ce9ac538ee
rayedsikder <rayedsikder33@gmail.com>
2022-07-25 10:10:53 +0600

    Add configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,49 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+extendEnvironment((hre) => {
+  const Web3 = require("web3");
+  hre.Web3 = Web3;
+
+  // hre.network.provider is an EIP1193-compatible provider.
+  hre.web3 = new Web3(hre.network.provider);
+});
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "alfajores",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:7545",
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 44787,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 42220,
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
 };
